3dcbba02b03bd21817a1056b34d50677c090791d
locher <locher.li@qq.com>
2024-09-09 22:07:56 +0800

    the root of the this project

---
+++ ProjuctManageSystem/src/Code/ConstantValue.java
@@ -0,0 +1,12 @@
+package Code;
+
+public class ConstantValue {
+    final public static String[] CATEGORY = new String[] {
+            "Food",
+            "Snack",
+            "Dish",
+            "Beverage",
+            "Toy",
+            "Handiwork"
+    };
+}
